24e52c4d03a4de4c6bd26f9401a2b593e7b8b858
0xheron <micahogden8@gmail.com>
2023-03-11 20:19:58 -0800

    Bugs

--- src/ai/minimax.cpp
+++ src/ai/minimax.cpp
@@ -4,23 +4,28 @@
 
 size_t search(int depth, Board& board)
 {
-    // if (depth == 0) return 0;
-    // size_t pos_c = 0;
-    // auto moves = board.generate_moves((Color) board.get_turn());
-    // if (depth == 1) 
-    // {
-    //     pos_c += moves.size();
-    // }
+    if (depth == 0) return 0;
+    size_t pos_c = 0;
+    auto piece_moves = board.generate_moves((Color) board.get_turn());
 
-    // for (auto m : moves)
-    // {
-    //     std::cout << "before move" << std::endl;
-    //     board.move(m);
-    //     std::cout << "after move " << std::endl;
-    //     pos_c += search(depth - 1, board);
-    //     board.unmove(m, true);
-    // }
+    for (auto x : piece_moves)
+    {
+        auto moves = extract_moves(x);
+        std::cout << "here" << std::endl;
+        // Crash on this line or entering moves loop
+        if (depth == 1) pos_c += moves.size();
+        std::cout << "her2e";
+        for (auto m : moves)
+        {
+            std::cout << "starting ";
+            board.move(m);
+            std::cout << "after moves ";
+            pos_c += search(depth - 1, board);
+            std::cout << "after search "; 
+            board.unmove(m);
+            std::cout << "after unmove ";
+        }
+    }
 
-    // return pos_c;
-    return 0;
+    return pos_c;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,12 @@
 int main(int argc, char** argv)
 {
     std::cout << "Chess AI" << std::endl;
-    std::string fen = argc == 2 ? argv[1] : "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    std::string fen = argc == 2 ? argv[1] : "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
 
     Disp disp;
     Board board;
     board.load_fen(fen);
-
-    auto moves = board.generate_moves((Color) board.get_turn());
-
-    Move dothisback;
-
-    for (auto x : moves)
-    {
-        auto split_moves = extract_moves(x);
-
-        std::cout << "start: " << (int) split_moves[0].start_pos << " end: " << (int) split_moves[0].end_pos << std::endl;
-        board.move(split_moves[0]);
-        dothisback = split_moves[0];
-        break;
-    }
-
-    print_bitset(board.all_pieces.to_ullong());
-
-    board.unmove(dothisback);
-
-    print_bitset(board.all_pieces.to_ullong());
+    
+    std::cout << search(2, board) << std::endl;
+    std::cout << "done" << std::endl;
 }
